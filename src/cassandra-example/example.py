f48d0cddaead12345d46721e8b90ffc183a7110f
Kacper Czajkowski <kacper.czajkowski@iteo.com>
2022-11-29 12:28:06 +0100

    comments

--- src/cassandra-example/example.py
+++ src/cassandra-example/example.py
@@ -1,7 +1,14 @@
 from cassandra.cluster import Cluster
 
 if __name__ == '__main__':
-    cluster = Cluster(['127.0.0.1'])
+    cluster = Cluster(['host.docker.internal'])
     session = cluster.connect()
 
     print(session.execute("SELECT release_version FROM system.local").one())
+    # ja tworze tabele
+
+    # ja dodaje dane do tabeli
+
+    # dominik kontrolnie może napisać sqlkę listującą te tabele
+
+    # odpalam selecta na bazunie
