1e885cf67222d4c62cc294266eb823632e48ea0f
Kacper Czajkowski <kacper.czajkowski@iteo.com>
2022-12-07 23:03:48 +0100

    changes

--- src/cassandra-example/example.py
+++ src/cassandra-example/example.py
@@ -1,10 +1,16 @@
 from cassandra.cluster import Cluster
 
-if __name__ == '__main__':
-    cluster = Cluster(['host.docker.internal'])
+CLUSTER_ADDRESS = 'host.docker.internal'
+
+def main():
+    # connecting to cluster
+    cluster = Cluster([CLUSTER_ADDRESS])
     session = cluster.connect()
 
     print(session.execute("SELECT release_version FROM system.local").one())
+
+    # create a new key space
+    session.execute
     # ja tworze tabele
 
     # ja dodaje dane do tabeli
@@ -12,3 +18,7 @@ if __name__ == '__main__':
     # dominik kontrolnie może napisać sqlkę listującą te tabele
 
     # odpalam selecta na bazunie
+
+
+if __name__ == '__main__':
+    main()
