09215f5d210007a80cf7d401feacd95b4519ebfd
Kacper Czajkowski <czajkowski.kacper99@gmail.com>
2022-12-08 00:21:18 +0100

    changes cd

--- src/cassandra-example/example.py
+++ src/cassandra-example/example.py
@@ -1,6 +1,13 @@
-from cassandra.cluster import Cluster
+import random
 
-CLUSTER_ADDRESS = 'host.docker.internal'
+from cassandra.cluster import Cluster
+from cassandra.protocol import SyntaxException
+
+CLUSTER_ADDRESS = 'localhost'
+KEY_SPACE = "mykeyspace"
+NAMES = ['Kacper', 'Dominik', 'Marcin', 'Krystian', 'Sebastian']
+EMAILS = ['test@test.pl', 'wsad@gmail.com', 'essa@mlodziez.eu', 'users@mat.umk.pl']
+
 
 def main():
     # connecting to cluster
@@ -9,15 +16,28 @@ def main():
 
     print(session.execute("SELECT release_version FROM system.local").one())
 
-    # create a new key space
-    session.execute
-    # ja tworze tabele
+    # tworzenie przestrzeni kluczy z replikacją typu simple strategy i replication_factor = 2
+    session.execute(
+        "CREATE KEYSPACE IF NOT EXISTS mykeyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '2' }",
+    )
+    # ustawienie przestrzeni kluczy, w ktorej bedziemy dzialac
+    session.set_keyspace(KEY_SPACE)
 
-    # ja dodaje dane do tabeli
+    # tworzenie tabeli
+    session.execute(
+        f'CREATE TABLE IF NOT EXISTS users (id int, name text, email text, PRIMARY KEY (id))'
+    )
 
-    # dominik kontrolnie może napisać sqlkę listującą te tabele
+    # dodawanie danych z parametrami
+    for i in range(10):
+        session.execute('INSERT INTO users(id, name, email) VALUES ( %(id)s, %(name)s, %(email)s )',
+                        {'id': i + 1, 'name': random.choice(NAMES), 'email': random.choice(EMAILS)})
 
-    # odpalam selecta na bazunie
+    # odczytywanie danych
+    results = session.execute('SELECT * FROM users')
+    for result in results:
+        print(result)
+
 
 
 if __name__ == '__main__':
